e09d536f6f884da364d5fc0fb975a8e0793219dd
Felix Holz <felix.holz@me.com>
2021-12-04 22:39:46 +0100

    working on apple watch integration

---
+++ ios/applewatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  applewatch WatchKit Extension
+//
+//  Created by Felix Holz on 04.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
