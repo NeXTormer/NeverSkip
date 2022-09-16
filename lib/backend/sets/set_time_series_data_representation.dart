54d06c86d29551d3bd77cfed26d8a54e02838d48
Felix Holz <felix.holz@me.com>
2022-09-16 03:51:30 +0200

    work on time series representation

--- lib/backend/sets/set_time_series_data_representation.dart
+++ lib/backend/sets/set_time_series_data_representation.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:collection';
 
+import 'package:collection/collection.dart';
 import 'package:frederic/backend/backend.dart';
 import 'package:frederic/backend/sets/frederic_set_manager.dart';
 import 'package:frederic/backend/sets/set_data_representation.dart';
@@ -10,16 +12,65 @@ class SetTimeSeriesDataRepresentation implements SetDataRepresentation {
   final FredericSetManager setManager;
   final FredericActivityManager activityManager;
 
+  List<TimeSeriesSet> list = [];
+
+  SplayTreeMap<DateTime, FredericSet> map = SplayTreeMap();
+
   FutureOr<void> initialize({required bool clearCachedData}) {
     // TODO: implement initialize
+
     throw UnimplementedError();
   }
 
+  Map<FredericActivity, List<FredericSet>> getSetsBetween(DateTime start,
+      DateTime end) {
+    return {};
+  }
+
+  Map<FredericActivity, List<FredericSet>> getLastWorkout({int maxDaysAgo = 2,
+    Duration maxTimeBetweenSets = const Duration(hours: 1)}) {
+    return {};
+  }
+
+
   void addSet(FredericActivity activity, FredericSet set) {
-    // TODO: implement addSet
+    if (set.timestamp.isAfter(list.last.set.timestamp)) {
+      list.add(TimeSeriesSet(activity.id, set));
+    } else {
+      //TODO: find where to put it using custom binary search
+
+      //list.insert(index, element)
+    }
   }
 
   void deleteSet(FredericActivity activity, FredericSet set) {
-    // TODO: implement deleteSet
+    int index = binarySearch(list, TimeSeriesSet(activity.id, set));
+    if (index == -1) return;
+
+    list.removeAt(index);
+  }
+}
+
+// no two Sets from one user should have the exact same timestamp,
+// so only comparing the timestamp should work
+class TimeSeriesSet implements Comparable<TimeSeriesSet> {
+  TimeSeriesSet(this.activityID, this.set);
+
+  String activityID;
+  FredericSet set;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is TimeSeriesSet &&
+              runtimeType == other.runtimeType &&
+              set.timestamp == other.set.timestamp;
+
+  @override
+  int get hashCode => set.timestamp.hashCode;
+
+  @override
+  int compareTo(TimeSeriesSet other) {
+    return set.timestamp.compareTo(other.set.timestamp);
   }
 }
