d0dcca4b5a0f259e329bcf42b9c8ea7e3f5e7477
Felix Holz <felix.holz@me.com>
2023-04-04 20:55:46 +0200

    add encrypted firebase files

---
+++ decrypt.sh
@@ -0,0 +1,2 @@
+gpg --output android/app/google-services.json --decrypt android/app/google-services.json.gpg
+gpg --output ios/Runner/GoogleService-Info.plist --decrypt ios/Runner/GoogleService-Info.plist.gpg
---
+++ encrypt.sh
@@ -0,0 +1,2 @@
+gpg --symmetric android/app/google-services.json
+gpg --symmetric ios/Runner/GoogleService-Info.plist
