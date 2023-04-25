38975de3f5cbdb5aa554ba851fc0d6379c4464f7
Felix Holz <felix.holz@me.com>
2023-04-26 00:18:14 +0200

    users are being displayed properly in the cms

--- firecms/src/App.tsx
+++ firecms/src/App.tsx
@@ -13,23 +13,16 @@ import "typeface-rubik";
 import "@fontsource/ibm-plex-mono";
 import {firebaseConfig} from "./firebase-config";
 
-type ChartDataPoint = {
-  metric: string,
-  value: number,
-  name: string,
-  description: string,
-}
-
 type FredericUser = {
-  name: String,
-  email: String,
-  image: String,
-  has_purchased: Boolean,
+  image: string,
+  name: string,
+  // email: string,
+  has_purchased: boolean,
   last_login: Date,
-  last_os: String,
-  last_os_version: String,
-  is_developer: Boolean,
-  should_reload_data: Boolean,
+  last_os: string,
+  last_os_version: string,
+  is_developer: boolean,
+  should_reload_data: boolean,
   trial_start: Date
 }
 
@@ -39,75 +32,60 @@ const usersCollection = buildCollection<FredericUser>({
   icon: "group",
   singularName: "User",
   properties: {
-    name: {
+    image: buildProperty({
+      dataType: "string",
+      name: "Image",
+      url: "image",
+      readOnly: true,
+
+    }),
+    name: buildProperty({
+      dataType: "string",
       name: "Name",
       validation: { required: false },
-      dataType: "string",
       readOnly: true,
-    },
-    email: {
-      name: "EMail",
-      dataType: "string",
-      validation: { required: false },
-      readOnly: true,
-    },
-    image: {
-      name: "Image",
-      dataType: "string",
-      readOnly: true,
-    },
-    has_purchased: {
-      name: "Has purchased",
+    }),
+    // email: buildProperty({
+    //   dataType: "string",
+    //   name: "EMail",
+    //   validation: { required: false },
+    //   readOnly: true,
+    // }),
+    has_purchased: buildProperty({
       dataType: "boolean",
-    },
-    last_login: {
+      name: "Has purchased",
+    }),
+    last_login: buildProperty({
       name: "Last login",
       dataType: "date",
+      model: "date_time",
       readOnly: true
-    }
-    last_os
-  }
-});
+    }),
+    should_reload_data: buildProperty({
+      name: "Should reload data",
+      dataType: "boolean"
+    }),
+    last_os: buildProperty({
+      name: "Last OS",
+      dataType: "string",
+      readOnly: true,
+    }),
+    last_os_version: buildProperty({
+      name: "Last OS verison",
+      dataType: "string",
+      readOnly: true,
+    }),
+    trial_start: buildProperty({
+      name: "Trial start date",
+      dataType: "date",
+      model: "date_time",
+      readOnly: true,
+    }),
+    is_developer: buildProperty({
+      name: "Is developer",
+      dataType: "boolean"
+    }),
 
-const chartDataPointsCollection = buildCollection<ChartDataPoint>({
-  path: "appdata",
-  name: "Datenpunkte",
-  icon: "Addchart",
-  singularName: "Datenpunkt",
-  properties: {
-    name: {
-      name: "Name",
-      validation: { required: false },
-      dataType: "string"
-    },
-    description: {
-      name: "Beschreibung",
-      dataType: "string",
-      markdown: true,
-      validation: { required: false },
-      columnWidth: 1000
-    },
-    metric: {
-      name: "Kennzahl",
-      dataType: "string",
-      validation: { required: true },
-      enumValues: {
-        exz: "Existenzzahl",
-        pz: "Prägungszahl",
-        fz: "Entfaltungszahl",
-        sz: "Schwingungszahl",
-        emz: "Empfindungszahl"
-      }
-    },
-    value: {
-      name: "Wert",
-      dataType: "number",
-      validation: {
-        required: true,
-        min: 1,
-        max: 9
-      },
-    }
   }
 });
 
@@ -123,20 +101,18 @@ export default function App() {
       throw Error("Stupid Flanders!");
     }
 
+    if(!user?.email?.startsWith("nextormer@gmail.com")) return false;
+
     console.log("Allowing access to", user?.email);
-    // This is an example of retrieving async data related to the user
-    // and storing it in the user extra field.
-    const sampleUserRoles = await Promise.resolve(["admin"]);
-    authController.setExtra(sampleUserRoles);
 
     return true;
 
   }, []);
 
   return <FirebaseCMSApp
-    name={"Hexoskop"}
+    name={"NeverSkip Fitness"}
     authentication={myAuthenticator}
-    collections={[chartDataPointsCollection]}
+    collections={[usersCollection]}
     firebaseConfig={firebaseConfig}
   />;
 }
