80a3ed574b7dc3756f4c16c13f4e9f1d58be00fb
Felix Holz <felix.holz@me.com>
2024-03-04 23:47:34 +0100

    release version 1.2.1

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/felix/Dev/SDKs/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/felix/Dev/Projects/frederic"
+export "FLUTTER_ROOT=/Users/felix/Developer/SDKs/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/felix/Developer/Projects/NeverSkip"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.2.0"
-export "FLUTTER_BUILD_NUMBER=24021702"
+export "FLUTTER_BUILD_NAME=1.2.1"
+export "FLUTTER_BUILD_NUMBER=24030401"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
